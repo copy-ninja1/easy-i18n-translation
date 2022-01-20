99b7839526ad458fe441ef561cd6ac61891d15a0
Emmanuel <emmajnr1000@gmail.com>
2022-01-20 03:24:08 +0100

    update git ignor

--- test.js
+++
@@ -1,6 +0,0 @@
-let easyTranslation = require('./index')
-
-test('translation', async () => {
-    let translatdData = await easyTranslation({ locales: ['de', 'fr'], characters: ['home'], apiTokensFilePath: './google-api-token.json' })
-    expect(translatdData).toEqual({ de: { home: 'Heimat' }, fr: { home: 'domicile' } });
-});
